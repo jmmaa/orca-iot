59de288075a4cb586b8b8375f43fbee39ad26cfd
arkhon7 <reviuy9@gmail.com>
2023-03-04 18:31:33 +0800

    changed baud rate to 9600

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use orca_iot::utils;
 
 fn main() {
-    const BAUD_RATE: u32 = 115200;
+    const BAUD_RATE: u32 = 9600;
     const TIMEOUT: u64 = 100 * 1000;
 
     utils::start(BAUD_RATE, TIMEOUT);
